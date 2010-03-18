dc967c8e120502e743af3da4e5a3e865f7beb67e
Paul James <paul@peej.co.uk>
2010-03-18 20:23:09 +0000

    Added comments

--- docroot/dispatch.php
+++ docroot/dispatch.php
@@ -1,10 +1,16 @@
 <?php
 
+// load Tonic library
 require_once '../lib/tonic.php';
+
+// example resource mounted at /example
 require_once '../resources/example.php';
+
+// example basic filesysystem resource collection mounted at /collection
 require_once '../resources/filesystem.php';
 require_once '../resources/filesystemcollection.php';
 
+// handle request
 $request = new Request();
 $resource = $request->loadResource();
 $response = $resource->exec($request);
