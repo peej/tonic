63203600b9058ec7a7b53b61301072d54114b40e
Paul James <paul@peej.co.uk>
2011-01-02 12:24:37 +0000

    Set error reporting to not include E_STRICT or E_DEPRECATED when running tests

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,11 @@
 <?php
 
+error_reporting(error_reporting() & ~2048 & ~8192); // Make sure E_STRICT and E_DEPRECATED are disabled
+
 require_once('simpletest/unit_tester.php');
 require_once('simpletest/reporter.php');
 
-$core = &new GroupTest('Core');
+$core = new GroupTest('Core');
 
 $core->addTestFile('request.php');
 $core->addTestFile('resource.php');
@@ -11,7 +13,7 @@ $core->addTestFile('response.php');
 $core->addTestFile('filesystem.php');
 $core->addTestFile('filesystemcollection.php');
 
-$test = &new GroupTest('Tonic');
+$test = new GroupTest('Tonic');
 $test->addTestCase($core);
 
 if (TextReporter::inCli()) {
