edc37ac92df568db0f5d53bc5f26e191075087b6
Paul James <paul@peej.co.uk>
2012-06-19 11:28:45 +0100

    Comments

--- src/Tonic/MetadataCache.php
+++ src/Tonic/MetadataCache.php
@@ -4,6 +4,8 @@ namespace Tonic;
 
 /**
  * Cache resource metadata between invocations
+ *
+ * This class serializes the resource metadata and writes it to disk for reading in a later request.
  */
 class MetadataCache {
 
@@ -13,16 +15,29 @@ class MetadataCache {
         $this->filename = $filename;
     }
 
+    /**
+     * Is there already cache file
+     * @return boolean
+     */
     public function isCached() {
         return is_readable($this->filename);
     }
 
+    /**
+     * Load the resource metadata from disk
+     * @return str[]
+     */
     public function load() {
         return unserialize(file_get_contents($this->filename));
     }
 
+    /**
+     * Save resource metadata to disk
+     * @param  str[] $resources Resource metadata
+     * @return boolean
+     */
     public function save($resources) {
-        file_put_contents($this->filename, serialize($resources));
+        return file_put_contents($this->filename, serialize($resources));
     }
 
 }
