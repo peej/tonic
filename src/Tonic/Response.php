67d52fe63cedb71465282264f84d553033446f22
Paul James <paul@peej.co.uk>
2012-06-19 22:59:58 +0100

    Response codes

--- src/Tonic/Response.php
+++ src/Tonic/Response.php
@@ -6,13 +6,63 @@ class Response {
 
     public $code, $body;
 
+    const OK = 200,
+        CREATED = 201,
+        NOCONTENT = 204,
+        MOVEDPERMANENTLY = 301,
+        FOUND = 302,
+        SEEOTHER = 303,
+        NOTMODIFIED = 304,
+        TEMPORARYREDIRECT = 307,
+        BADREQUEST = 400,
+        UNAUTHORIZED = 401, 
+        FORBIDDEN = 403, 
+        NOTFOUND = 404, 
+        METHODNOTALLOWED = 405,
+        NOTACCEPTABLE = 406,
+        GONE = 410,
+        LENGTHREQUIRED = 411,
+        PRECONDITIONFAILED = 412,
+        UNSUPPORTEDMEDIATYPE = 415,
+        IMATEAPOT = 418,
+        ENHANCEYOURCALM = 420,
+        INTERNALSERVERERROR = 500;
+
+    /**
+     * Map of HTTP response codes
+     * @var str[]
+     */
+    public $codes = array(
+        Response::OK => 'OK',
+        Response::CREATED => 'Created',
+        Response::NOCONTENT => 'No Content', 
+        Response::MOVEDPERMANENTLY => 'Moved Permanently',
+        Response::FOUND => 'Found', 
+        Response::SEEOTHER => 'See Other', 
+        Response::NOTMODIFIED => 'Not Modified', 
+        Response::TEMPORARYREDIRECT => 'Temporary Redirect', 
+        Response::BADREQUEST => 'Bad Request', 
+        Response::UNAUTHORIZED => 'Unauthorized', 
+        Response::FORBIDDEN => 'Forbidden', 
+        Response::NOTFOUND => 'Not Found', 
+        Response::METHODNOTALLOWED => 'Method Not Allowed',
+        Response::NOTACCEPTABLE => 'Not Acceptable',
+        Response::GONE => 'Gone',
+        Response::LENGTHREQUIRED => 'Length Required',
+        Response::PRECONDITIONFAILED => 'Precondition Failed',
+        Response::UNSUPPORTEDMEDIATYPE => 'Unsupported Media Type',
+        Response::IMATEAPOT => 'I\'m A Teapot',
+        Response::UNSUPPORTEDMEDIATYPE => 'Enhance Your Calm',
+        Response::INTERNALSERVERERROR => 'Internal Server Error'
+    );
+
     function __construct($code = 204, $body = '') {
         $this->code = $code;
         $this->body = $body;
     }
 
     function output() {
-        header('HTTP/1.1 '.$this->code);
+        header('HTTP/1.1 '.$this->code.' '.$this->codes[$this->code]);
         echo $this->body;
     }
 
