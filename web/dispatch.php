88d173497ecc915c3fdae924620079196b2b0c0f
Paul James <paul@peej.co.uk>
2012-08-19 14:08:07 +0100

    Send exception message with 500 error response

--- web/dispatch.php
+++ web/dispatch.php
@@ -34,7 +34,7 @@ try {
     $response->wwwAuthenticate = 'Basic realm="My Realm"';
 
 } catch (Tonic\Exception $e) {
-    $response = new Tonic\Response(500);
+    $response = new Tonic\Response(500, $e->getMessage());
 }
 
 #echo $response;
